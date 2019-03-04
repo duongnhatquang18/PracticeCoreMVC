44c9196a311314c8bf6de112def04969c923ea5b
dnquang <duongnhatquang18@gmail.com>
2019-03-04 21:38:38 +0700

    Implement Admin Area for project.

--- PracticeCoreMVC/Startup.cs
+++ PracticeCoreMVC/Startup.cs
@@ -107,6 +107,9 @@ namespace PracticeCoreMVC
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "Admin",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
 
             //dbInitializer.Seed().Wait();
